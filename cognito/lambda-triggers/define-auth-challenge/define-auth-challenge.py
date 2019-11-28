2ab7516cd2202dad126dc9f37e04a774db637703
Christoph <cs@system32.de>
2019-11-28 14:33:07 +0100

    last changes

--- cognito/lambda-triggers/define-auth-challenge/define-auth-challenge.py
+++ cognito/lambda-triggers/define-auth-challenge/define-auth-challenge.py
@@ -5,7 +5,6 @@ def handler(event, context):
     print('event_in: {}'.format(json.dumps(event)))
 
     if event['request']['session'] and len(event['request']['session']) >= 3 and event['request']['session'][0]['challengeResult'] == False:
-        #event['request']['session'].slice(-1)[0].challengeResult == False:
         #// The user provided a wrong answer 3 times; fail auth
         event['response']['issueTokens'] = False
         event['response']['failAuthentication'] = True
